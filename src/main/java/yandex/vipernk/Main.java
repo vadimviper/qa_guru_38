d82c49af495dc36ad6a21af6bd75dd500cd86745
Vadim Khabibrakhmanov <97433772+vadimviper@users.noreply.github.com>
2025-10-04 01:55:42 +0300

    Update Main.java

--- src/main/java/yandex/vipernk/Main.java
+++ src/main/java/yandex/vipernk/Main.java
@@ -2,7 +2,8 @@ package yandex.vipernk;
 
 public class Main {
 
-    private static String msg = "Hello qa.quru!!!!!!!"
+    private static String msg = "Hello qa.quru!";
+    
     public static void main(String[] args) {
         System.out.println(msg);
     }
